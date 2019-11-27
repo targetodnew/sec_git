d5875b00e7d47a201558d21c99ebe6211cfd6c17
Alex <alex@gmail.com>
2019-11-27 09:35:08 +0200

    added class

--- main.cpp
+++ main.cpp
@@ -2,11 +2,24 @@
 
 using namespace std;
 
+
+class New
+{
+    int a;
+    int b;
+public:
+    void show(){ cout << a << b <<endl;}
+};
+
+
 int main()
 {
     cout << "Hello GIT" << endl;
 
     cout << "test git" <<endl;
+
+
+
 
     return 0;
 }
